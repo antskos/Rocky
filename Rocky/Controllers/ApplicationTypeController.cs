88eb3de7302809c45094063c7302ecf335549a96
antskos <antskos@gmail.com>
2022-09-26 12:30:13 +0300

    Добавьте файлы проекта.

---
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: ApplicationTypeController
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> appTypes = _db.ApplicationType;
+            return View(appTypes);
+        }
+
+      
+        // GET: ApplicationTypeController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ApplicationTypeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType appType)
+        {
+            _db.ApplicationType.Add(appType);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+          
+        }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var obj = _db.ApplicationType.Find(id);
+
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType appType)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(appType);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(appType);
+        }
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var obj = _db.ApplicationType.Find(id);
+
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var appType = _db.ApplicationType.Find(id);
+
+            if (appType == null)
+                return NotFound();
+
+            _db.ApplicationType.Remove(appType);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using System.Collections.Generic;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.Category; 
+            return View(categories);
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category cat)
+        {
+            if (ModelState.IsValid) 
+            {
+                _db.Category.Add(cat);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cat);
+        }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var obj = _db.Category.Find(id);
+
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(cat);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cat);
+        }
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var obj = _db.Category.Find(id);
+
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var cat = _db.Category.Find(id);
+
+            if (cat == null)
+                return NotFound();
+
+            _db.Category.Remove(cat); 
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
